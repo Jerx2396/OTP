f47cb8cdfd96dd0ac60dcb6e4e5b8793b326bb96
jerxni2396 <jeremy.benocilla@csucc.edu.ph>
2023-03-09 16:53:58 +0800

    2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const submit = document.querySelector(".submit"); 
-submit.addEventListener('click',(e)=>{ 
-    e.preventDefault()
-    const input = document.querySelector(".input").value
-    if(isNaN(parseInt(input)))alert("Incorrect code!")
-    else if(input.length<6)alert("Input is too short!")
-    else if(input.length>6)alert("Input is too long!")
-    else {
-        alert("Input successful!")
-        location.reload()
-    }
+const nums = [document.getElementById("num1"), document.getElementById("num2"), document.getElementById("num3"), document.getElementById("num4"), document.getElementById("num5"), document.getElementById("num6")];
+nums.forEach((value) => {
+    value.readOnly = true
 })
+nums[0].readOnly = false
+nums.forEach((value, index) => {
+  value.addEventListener('input', () => {
+    const regex = /\d/
+    if(regex.test(nums[index].value)){
+        // Disable the current input field
+        nums[index].readOnly = true;
+        nums[index].disabled = true
+
+        // Move focus to the next input field if it exists
+        if (nums[index+1]) {
+        nums[index+1].readOnly = false;
+        nums[index+1].focus();
+        }
+    }else nums[index].value = ""
+    
+  });
+});
